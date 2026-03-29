58619e09fc6d7c7c97bd615edf96c5eb7da11dc7
Ganga200-glitch <gangasunil2005@gmail.com>
2026-03-29 17:55:45 +0530

    What if duplication removed

--- frontend/src/components/WhatIfPanel.jsx
+++ frontend/src/components/WhatIfPanel.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
 
 export default function WhatIfPanel({ settings = {}, onChange }) {
-  const toggle = (key) => {
-    const next = { ...settings, [key]: !settings[key] };
-    onChange && onChange(next);
+  const defaultSettings = {
+    rain: false,
+    blockage: false,
+    peak: false
   };
 
-  return (
-    <div style={{ marginTop: 12 }}>
-      <strong>What-If Controls</strong>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: 8, marginTop: 8 }}>
-        <label style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <span>Rain (increase congestion)</span>
-          <input type="checkbox" checked={!!settings.rain} onChange={() => toggle('rain')} />
-        </label>
+  const handleRun = () => {
+    onChange && onChange(defaultSettings);
+  };
 
-        <label style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <span>Road Blockage (disable node)</span>
-          <input type="checkbox" checked={!!settings.blockage} onChange={() => toggle('blockage')} />
-        </label>
-
-        <label style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <span>Peak Hour Surge</span>
-          <input type="checkbox" checked={!!settings.peak} onChange={() => toggle('peak')} />
-        </label>
-      </div>
-    </div>
-  );
+  
 }
